8d34b675285d93690ff7570c92d45f31f149c92f
Adam Siefker <siefker@amazon.com>
2026-03-10 22:57:29 +0000

    Builder setter methods.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,6 +2,7 @@ use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::Fields::Named;
 use syn::{parse_macro_input, parse_quote, DeriveInput, Error, Field, Ident, Type};
+use syn::__private::TokenStream2;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -11,18 +12,27 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_name = format_ident!("{}Builder", ast.ident);
     // println!("{}", builder_name);
 
-    let builder_fields = match build_fields(&ast) {
+    let fields = match extract_fields(&ast) {
         Ok(value) => value,
         Err(err) => { return err.to_compile_error().into();}
     };
-
-    let field_idents: Vec<&Option<Ident>> = builder_fields.iter().map(|f| &f.ident).collect();
-
+    let builder_fields = make_field_option(&fields);
+    let field_idents: Vec<&Option<Ident>> = fields.iter().map(|f| &f.ident).collect();
+    let builder_methods: Vec<TokenStream2> = fields.iter().map(|f| make_builder_method(f)).collect();
     let generated = quote! {
         pub struct #builder_name {
             #(#builder_fields),*
         }
 
+        impl #builder_name {
+            #(#builder_methods)*
+
+            // pub fn build(self) -> #name {
+            //     #name {
+            //         #(#field_idents: self.#field_idents.unwrap()),*
+            //     }
+            // }
+        }
        impl #name {
             pub fn builder() -> #builder_name {
                 #builder_name {
@@ -36,17 +46,21 @@ pub fn derive(input: TokenStream) -> TokenStream {
     //eprintln!("TOKENS: {}", generated);
     generated.into()
 }
+fn make_field_option(fields: &[Field]) -> Vec<Field>{
+    fields.iter().map(|f|{
+        let mut nf = f.clone();
+        make_field_optional(&mut nf);
+        nf
+    }).collect()
+}
 
-fn build_fields(ast: &DeriveInput) -> syn::Result<Vec<Field>> {
+fn extract_fields(ast: &DeriveInput) -> syn::Result<Vec<Field>> {
     if let syn::Data::Struct(syn::DataStruct { fields, .. }) = &ast.data {
         match fields {
             Named(fields_named) => {
-                let mut builder_fields = Vec::new();
-                fields_named.named.pairs().for_each(|field| {
-                    let mut f: Field = (*field.value()).clone();
-                    make_field_optional(&mut f);
-                    builder_fields.push(f);
-                });
+                let builder_fields = fields_named.named.pairs().map(|field| {
+                    (*field.value()).clone()
+                }).collect();
                 return Ok(builder_fields);
             },
             _ => {
@@ -69,10 +83,22 @@ fn make_field_optional(field: &mut Field) {
 
     // Use parse_quote! to construct the new Type::Path for Option<T>
     // The `#inner_type` fragment is replaced by the actual type
+    // TODO Skip if already optional...
     let option_type: Type = parse_quote! {
         Option<#inner_type>
     };
 
     // Update the field's type
     field.ty = option_type;
+}
+
+fn make_builder_method(field: &Field) -> TokenStream2 {
+    let ident = &field.ident;
+    let typ = &field.ty;
+    quote!{
+        pub fn #ident(&mut self, #ident: #typ) -> &mut Self {
+            self.#ident = Some(#ident);
+            self
+        }
+    }
 }
--- builder/tests/03-call-setters.rs
+++ builder/tests/03-call-setters.rs
@@ -24,6 +24,6 @@ fn main() {
     let mut builder = Command::builder();
     builder.executable("cargo".to_owned());
     builder.args(vec!["build".to_owned(), "--release".to_owned()]);
-    builder.env(vec![]);
-    builder.current_dir("..".to_owned());
+    builder.env(vec![])
+        .current_dir(".".to_owned());
 }
